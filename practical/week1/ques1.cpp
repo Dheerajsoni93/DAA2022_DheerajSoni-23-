8c8163fcb890794b1ebd4891dc74bf3f89a2e300
Dheeraj_Soni <82114565+Dheerajsoni93@users.noreply.github.com>
2022-03-05 15:25:14 +0530

    week 1 questions submission

---
+++ practical/week1/ques1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, key = -1, i;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements: ";
+    for(int i=0 ; i<n ; i++)
+        cin>>arr[i];
+    cout<<"Enter the key: ";
+    cin>>key;
+    for(i=0 ; i<n ; i++)
+    {
+        if(key == arr[i])
+        {
+            cout<<"Element found!"<<endl;
+            break;
+        }
+    }
+    if(i == n)
+    {
+        cout<<"Element not found!"<<endl;
+        cout<<"Total number of comparison: "<<i<<endl;
+    }
+    else{
+    cout<<"Total number of comparison: "<<i+1<<endl;
+    }
+    return 0;
+}
