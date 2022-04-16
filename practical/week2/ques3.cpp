6f68f53d64244fa9313c7be500ccfe9e1d027c02
Dheeraj_Soni <82114565+Dheerajsoni93@users.noreply.github.com>
2022-04-16 22:35:19 +0530

    difference of two element equal to key

--- practical/week2/ques3.cpp
+++ practical/week2/ques3.cpp
@@ -3,33 +3,26 @@ using namespace std;
 
 int main()
 {
-    int com=0, n, key, j;
+    int t;
+    cin>>t;
+    while(t!=0)
+    {
+
+    int n,key;
     cout<<"Enter the size of array: ";
     cin>>n;
     int arr[n];
-    //imput the elements in array
-    cout<<"Enter the elements: ";
-    for(int i=0 ; i<n ; i++)
+    cout<<"Enter the size of array: ";
+    for(int i=0 ;i<n ;i++)
         cin>>arr[i];
+    cout<<"Enter the key: ";
+    cin>>key;
 
-    //insertion sort
-    for(int i=1 ; i<n ; i++)
-    {
-        key = arr[i];
-        j = i-1;
-
-        while(j>=0 && arr[j] > key)
-        {
-            arr[j+1] = arr[j];
-            j--;
-            com++;
-        }
-        arr[j+1] = key;
+    for(int i=0 ;i<n-1 ; i++)
+        for(int j=i+1 ; j<n ;j++)
+            if(abs(arr[i]- arr[j]) == key)
+                    cout<<"indexes are"<<i<<j<<" ";
+    t--;
     }
-
-    //printing array
-    for(int i=0 ; i<n ; i++)
-        cout<<arr[i]<<" ";
-    cout<<"\nTotal number of comparison: "<<com;
-    
+    return 0;
 }
